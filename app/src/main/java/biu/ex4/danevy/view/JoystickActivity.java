ca4591a1f10ec7bb22543693d13d8eda762046bc
evyatar18 <evyatar38@gmail.com>
2019-06-20 10:42:37 +0300

    prepared activities

---
+++ app/src/main/java/biu/ex4/danevy/view/JoystickActivity.java
@@ -0,0 +1,15 @@
+package biu.ex4.danevy.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import biu.ex4.danevy.R;
+
+
+public class JoystickActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joystick);
+    }
+}
