878dcb8510ba3e9fbf614e0e795ec7d1e7ecc40e
evyatar18 <evyatar38@gmail.com>
2019-06-23 14:22:34 +0300

    finished joystick view code

--- app/src/main/java/biu/ex4/danevy/view/JoystickActivity.java
+++ app/src/main/java/biu/ex4/danevy/view/JoystickActivity.java
@@ -2,14 +2,29 @@ package biu.ex4.danevy.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
 import biu.ex4.danevy.R;
+import biu.ex4.danevy.view.joystick.Joystick;
+import biu.ex4.danevy.view.joystick.OnJoystickValueChange;
 
 
 public class JoystickActivity extends AppCompatActivity {
+
+    private class Notified implements OnJoystickValueChange {
+
+        @Override
+        public void onChanged(float x, float y) {
+            Log.i("vc", "x=" + x + ", y=" + y);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joystick);
+
+        Joystick joy = findViewById(R.id.joystick);
+        joy.setValueChangeListener(new Notified());
     }
 }
